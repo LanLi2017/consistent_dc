406f963725f01d8be8f6955df2b919afcb5a0ea4
Lan Li <lilan.scut@gmail.com>
2024-09-21 04:26:03 +0800

    update

--- m1_disjoint.py
+++ m1_disjoint.py
@@ -13,7 +13,6 @@ def exe_process(process,df):
         df_cur = df.copy()
         log_change = []
         func = f['func']  # Get the function
-        print(func)
         log_change.append(func.__name__)
         params = f['params']  # Get the parameters
         df = func(df, **params)  # Call the function with the DataFrame and parameters
@@ -32,16 +31,27 @@ if __name__ == '__main__':
         "currency_symbol": ["s", "$", "", "", "$", "$"]
     }
     df = pd.DataFrame(data_in)
+    print(f"The original data input:")
+    print(df)
     df.to_csv("m1_origin.csv")
     chars_to_remove = r"[()?:;,.]"
 
     df1 = df.copy()
     # List of functions with their respective parameters, including the DataFrame
+    print(">>>>>>>Start Process P1>>>>>>>")
+    
     process1 = [
         {'func': upper_op, 'params': {'col': 'event'}},
         {'func': rm_char, 'params': {'col': 'event', 'chars_to_remove': r"[()?:;,.]"}}
     ]
-
+    print(f"{process1}")
+    # Loop through the functions and pass the DataFrame through each function
+    log_changes_p1 = exe_process(process1, df1)
+    print("The executed changes by process P1")
+    pprint(log_changes_p1)
+    print(">>>>>END P1>>>>>")
+    
+    print(">>>>>>>>Start Process P2>>>>>>>>")
     df2 = df.copy()
     process2 = [
         {'func': rm_char, 'params': {'col': 'venue', 'chars_to_remove': r"[()?:;,.]"}},
@@ -50,13 +60,9 @@ if __name__ == '__main__':
                                                                 'COM': 'COMMERCIAL'
                                                                 }}}
     ]
-
-
-    # Loop through the functions and pass the DataFrame through each function
-    log_changes_p1 = exe_process(process1, df1)
+    print(f"{process1}")
     log_changes_p2 = exe_process(process2, df2)
-    pprint(log_changes_p1)
-    print(">>>>>END P1>>>>>")
+    print("The executed changes by process P2")
     pprint(log_changes_p2)
     print(">>>>>END P2>>>>>")
     
